d40c0315e6bf17e41b0125288a061ba042444fd7
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-09 17:27:51 -0300

    powershell script to run the bot

---
+++ run.ps1
@@ -0,0 +1,17 @@
+# Gets the location of this file
+$ScriptPath = ($MyInvocation.MyCommand).Path
+
+# Gets the location of the parent container (folder)
+$ScriptPath = Split-Path -Parent $ScriptPath
+
+# Advanced logging /s
+Write-Output "Switching to: $ScriptPath"
+
+# Change directory to the folder of this project
+cd $ScriptPath
+
+# Activates the virtual environment
+.\venv\Scripts\activate
+
+# Runs the bot script
+py .\main.py

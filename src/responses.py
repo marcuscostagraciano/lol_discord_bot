fed9e2c73aac8b79bf654090ba9be77b1aa62833
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-09 14:48:13 -0300

    feat(role abbreviation): first character defines the role

--- src/responses.py
+++ src/responses.py
@@ -1,14 +1,41 @@
+from discord import Message, File
+
+from os import remove as rm
+from typing import Final, NoReturn
+
 from . import get_screenshot
 
-# ver rotas
+role_abbreviation: Final[dict] = {
+    "T": "TOP",
+    "J": "JUNGLE",
+    "M": "MID",
+    "A": "ADC",
+    "S": "SUPPORT"
+}
 
-def get_reponse(user_input: str) -> str:
-    user_input = user_input.upper().split(" ")
-    champ_name: str = user_input[0]
-    role: str = user_input[1]
 
-    try:
-        get_screenshot.get_screenshoot(champ_name, role)
-        return f"{champ_name} {role}"
-    except Exception as e:
-        print(e)
+async def get_reponse(*, BOT_COMMAND: str, msg: Message,
+                      user_msg: str) -> NoReturn:
+    user_msg: str = user_msg[len(BOT_COMMAND) + 1:]
+    user_input: list[str] = user_msg.upper().split(" ")
+
+    match user_input[0]:
+        case "?":
+            await msg.channel.send("Use !LOLKO {nome do campeao} {lane}")
+        case _:
+            champ_name: str = user_input[0]
+            role: str = user_input[1]
+
+            if role in role_abbreviation:
+                role = role_abbreviation[role]
+
+            filename: str = f"{champ_name} {role}.png"
+
+            try:
+                get_screenshot.get_screenshoot(champ_name, role)
+                await msg.channel.send(f"<@{msg.author.id}>, here's the build for " +
+                                       f"{champ_name} {role}:",
+                                       file=File(filename))
+                rm(filename)
+            except Exception as e:
+                print(e)

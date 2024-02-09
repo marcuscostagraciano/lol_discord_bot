a597a73993760c6f9b3377b6fdd1477b2bf54509
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-09 15:36:14 -0300

    better message handling

--- src/responses.py
+++ src/responses.py
@@ -17,22 +17,22 @@ role_abbreviation: Final[dict] = {
 async def get_reponse(*, BOT_COMMAND: str, msg: Message,
                       user_msg: str) -> NoReturn:
     user_msg: str = user_msg[len(BOT_COMMAND) + 1:]
-    user_input: list[str] = user_msg.upper().split(" ")
+    # user_input: list[str] = user_msg.upper().split(" ")
 
-    match user_input[0]:
-        case "?":
+    match user_input := user_msg.upper().split(" "):
+        case ["?"] | ["?", _]:
             await msg.channel.send("Use !LOLKO {nome do campeao} {lane}")
 
         case _:
-            champ_name: str = user_input[0]
-            role: str = user_input[1]
-
-            if role in role_abbreviation:
-                role = role_abbreviation[role]
-
-            filename: str = f"{champ_name} {role}.png"
-
             try:
+                champ_name: str = user_input[0]
+                role: str = user_input[1]
+
+                if role in role_abbreviation:
+                    role = role_abbreviation[role]
+
+                filename: str = f"{champ_name} {role}.png"
+
                 get_screenshot.get_screenshoot(champ_name, role)
                 await msg.channel.send(f"<@{msg.author.id}>, here's the build for " +
                                        f"{champ_name} {role}:",
@@ -40,3 +40,4 @@ async def get_reponse(*, BOT_COMMAND: str, msg: Message,
                 rm(filename)
             except Exception as e:
                 print(e)
+                await msg.channel.send(f"[<@{msg.author.id}>]: {e}")

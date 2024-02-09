af5adb9a83b20558e48696dfa9b6dbe8bdd1c529
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-09 13:31:13 -0300

    refator(filename): screenshot name has it's own var

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ async def send_message(msg: Message, user_msg: str) -> None:
     if user_msg.startswith(BOT_COMMAND):
         user_msg = user_msg[len(BOT_COMMAND) + 1:]
 
-    try:
-        response: str = get_reponse(user_msg)
-        await msg.channel.send(f"{msg.author}, here's the build for {response}:",
-                               file=discord.File(f'{response}.png'))
-    except Exception as e:
-        print(e)
-
+        try:
+            response: str = get_reponse(user_msg)
+            filename: str = f"{response}.png"
+            await msg.channel.send(f"{msg.author}, here's the build for {response}:",
+                                   file=discord.File(filename))
+            os.remove(filename)
+        except Exception as e:
+            print(e)
 
 @client.event
 async def on_ready() -> None:

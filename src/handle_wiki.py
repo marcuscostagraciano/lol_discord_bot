1e3dd7314e6471aed43ea03ba6104e931853cf97
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-10 10:48:53 -0300

    feat: handle wiki requests

--- src/handle_wiki.py
+++ src/handle_wiki.py
@@ -1,10 +1,41 @@
-from discord import Message
+from discord import File, Message
 
-from typing import NoReturn
+from os import remove as rm
+from typing import Final, NoReturn
 
 from .get_screenshot import get_screenshoot
 
+CLASS_ABBREVIATION: Final[dict] = {
+    "P": "skill skill_innate",
+    "PASSIVA": "skill skill_innate",
+    "Q": "skill skill_q",
+    "W": "skill skill_w",
+    "E": "skill skill_e",
+    "R": "skill skill_r",
+    "U": "skill skill_r",
+    "ULT": "skill skill_r",
+}
+
 
 async def wiki_handler(msg: Message, champ_name: str,
-                       *opts: list[str]) -> NoReturn:
-    ...
+                       opts: list[str]) -> NoReturn:
+    raise NotImplementedError("WIP")
+
+    champ_name = champ_name.lower().capitalize()
+    link: str = f"https://leagueoflegends.fandom.com/wiki/{champ_name}/LoL"
+
+    try:
+        for opt in opts:
+            filename: str = f"{champ_name} {opt}.png"
+            print(f"{link=}")
+            print(f"{filename=}")
+            print(f"{CLASS_ABBREVIATION[opt]=}")
+
+            get_screenshoot(link, filename, CLASS_ABBREVIATION[opt])
+            await msg.channel.send(f"<@{msg.author.id}>, here's the wiki for " + # NOQA
+                                   f"{filename}:",
+                                   file=File(filename))
+            rm(filename)
+
+    except Exception as e:
+        raise e

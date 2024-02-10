d0c48bc94a277fa52350ff9e1df6fa9bf01edb32
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-10 10:46:12 -0300

    feat: handles both build and wiki(wip)

--- src/get_screenshot.py
+++ src/get_screenshot.py
@@ -2,14 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
-from typing import Final, NoReturn
+from typing import Final
 
 
-def get_screenshoot(opt: str, link: str, filename: str, *html_class) -> None:
+def get_screenshoot(link: str, filename: str, html_class: str) -> None:
     options = webdriver.ChromeOptions()
 
-    ARGUMENTS: Final[list] = ["--incognito", "--headless",
-                              "window-size=1600,1600"]
+    ARGUMENTS: Final[list] = ["--headless", "window-size=1600,1600"]
     for argument in ARGUMENTS:
         options.add_argument(argument)
 
@@ -17,14 +16,8 @@ def get_screenshoot(opt: str, link: str, filename: str, *html_class) -> None:
     driver.get(link)
 
     try:
-        match opt:
-            case "build":
-                data = driver.find_element(By.CLASS_NAME, html_class[0])
-                data.screenshot(filename)
-
-            case "wiki":
-                data = driver.find_element(By.CLASS_NAME, html_class[0])
-                data.screenshot(filename)
+        data = driver.find_element(By.CLASS_NAME, html_class)
+        data.screenshot(filename)
 
     except NoSuchElementException:
         raise ValueError("Escrita errada ou dados insuficientes, tente novamente")

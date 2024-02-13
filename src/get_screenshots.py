e3aafa2984f2f7cbffcc8a95f21879ca64f243dc
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-13 20:30:53 -0300

    renamed

---
+++ src/get_screenshots.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+import logging
+from typing import Final
+
+
+def get_build(link: str, filename: str, html_class: str) -> None:
+    logging.info(f"Working on: {link}")
+    options = webdriver.ChromeOptions()
+
+    ARGUMENTS: Final[list] = ["--headless", "window-size=1600,1600"]
+    for argument in ARGUMENTS:
+        options.add_argument(argument)
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(link)
+
+    try:
+        data = driver.find_element(By.CLASS_NAME, html_class)
+        data.screenshot(filename)
+
+    except NoSuchElementException:
+        raise ValueError("Elemento não encontrado, tente novamente.")
+
+    except TimeoutException:
+        raise TimeoutError("Timeout")
+
+    driver.quit()

ba4de92667607e10549d9dab87adea0e97b0784e
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-02-13 21:05:54 -0300

    for future implementations

--- src/handle_wiki.py
+++ src/handle_wiki.py
@@ -4,39 +4,41 @@ import logging
 from os import remove as rm
 from typing import Final, NoReturn
 
-from .get_screenshot import get_screenshoot
+# from .get_screenshots import get_wiki
 
 CLASS_ABBREVIATION: Final[dict] = {
-    "P": "skill skill_innate",
-    "PASSIVA": "skill skill_innate",
-    "Q": "skill skill_q",
-    "W": "skill skill_w",
-    "E": "skill skill_e",
-    "R": "skill skill_r",
-    "U": "skill skill_r",
-    "ULT": "skill skill_r",
+    "P": "skill_innate",
+    "PASSIVA": "skill_innate",
+    "Q": "skill_q",
+    "W": "skill_w",
+    "E": "skill_e",
+    "R": "skill_r",
+    "U": "skill_r",
+    "ULT": "skill_r",
 }
 
 
 async def wiki_handler(msg: Message, champ_name: str,
                        opts: list[str]) -> None | NoReturn:
-    raise NotImplementedError("WIP\n" +
-                              f"passed args: {champ_name=}\n{opts=}")
+    raise NotImplemented("WIP (maybe someday)")
 
     champ_name = champ_name.lower().capitalize()
     link: str = f"https://leagueoflegends.fandom.com/wiki/{champ_name}/LoL"
 
+    logging.info(f"Getting the wiki for: {champ_name} {opts}")
+
     try:
         for opt in opts:
-            filename: str = f"{champ_name} {opt}.png"
-            print(f"{link=}")
-            print(f"{filename=}")
-            print(f"{CLASS_ABBREVIATION[opt]=}")
+            logging.debug(f"Wiki for: {champ_name}\n" +
+                          f"{link=}")
 
-            get_screenshoot(link, filename, CLASS_ABBREVIATION[opt])
-            await msg.channel.send(f"<@{msg.author.id}>, here's the wiki for " + # NOQA
-                                   f"{filename}:",
-                                   file=File(filename))
+            filename: str = f"{champ_name} {opt}.png"
+
+            get_wiki(link, filename, CLASS_ABBREVIATION[opt])
+            await msg.channel.send(
+                f"<@{msg.author.id}>, here's the wiki for " +
+                f"{filename}:",
+                file=File(filename))
             rm(filename)
 
     except Exception as e:
